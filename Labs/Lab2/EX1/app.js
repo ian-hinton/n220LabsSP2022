71385cc27c14ba486c12132a411c6c3e5c5c36ec
ian-hinton <ian_hinton@yahoo.com>
2022-01-26 20:15:49 -0500

    comments

--- Labs/Lab2/EX1/app.js
+++ Labs/Lab2/EX1/app.js
@@ -1,11 +1,19 @@
+//Ian Hinton
+//N220
+//1/27/2022
+//This program outputs a number to the console every draw call
+
+//list varible
 let i = 0;
 
-
+//setup
 function setup() {
     createCanvas(600, 600);
 }
 
+//draw
 function draw() {
+//increase varible by 1 and call console.log
     i++;
     console.log(i);
 }
--- Labs/Lab2/EX2/app.js
+++ Labs/Lab2/EX2/app.js
@@ -1,11 +1,22 @@
+//Ian Hinton
+//N220
+//1/27/2022
+//This program draws a circle when mouse is pressed
+//if mouse is pressed on left side circle is blue
+//is mouse is pressed on right side circle is red
+
+//setup
 function setup() {
     createCanvas(400, 300);
+//grey background
     background(155);
 }
 
+//draw
 function draw() {
     fill(0);
-
+//if-else statement blue circle when mousex is less than 200
+//else red circle
     if(mouseIsPressed) {
         if(mouseX < 200) {
             fill(0, 0, 255);
