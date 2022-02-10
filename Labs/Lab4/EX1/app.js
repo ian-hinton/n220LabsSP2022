07da32c7fdedb821cab8a0f61e5147df0d79f924
ian-hinton <ian_hinton@yahoo.com>
2022-02-10 13:08:19 -0500

    comments

--- Labs/Lab4/EX1/app.js
+++ Labs/Lab4/EX1/app.js
@@ -1,3 +1,10 @@
+//Ian Hinton
+//N220
+//2/10/2022
+
+//This program invokes an object in order to create a rectangle
+
+//create object
 let graphic = {
     x: 100,
     y: 100,
@@ -6,17 +13,21 @@ let graphic = {
     height: 100,
 }
 
+//setup
 function setup(){
     createCanvas(400, 400);
     background(155);
 }
 
+//draw
 function draw(){
     fill("BLUE");
+    //invoke car() function
     car(graphic);
     
 }
 
+//car function
 function car(objbox){
     rect(objbox.x, objbox.y, objbox.width, objbox.height);
     console.log(objbox);
--- Labs/Lab4/EX2/app.js
+++ Labs/Lab4/EX2/app.js
@@ -1,3 +1,11 @@
+//Ian Hinton
+//N220
+//2/10/2022
+
+//This program animates a white circle increasing or decreasing in the x-direction
+//depending is mouse is pressed 
+
+//create pet object
 let pet = {
     x: 0,
     y: 0,
@@ -5,15 +13,19 @@ let pet = {
     offsetX: 0,
 }
 
+//setup
 function setup() {
     createCanvas(600,600);
     background(155);
 }
 
+//draw
 function draw() {
+    //if mouse down, offset - 1
     if(mouseIsPressed && pet.offsetX > 0) {
         pet.offsetX--;
     }
+    //if mouse up, offset + 1
     if(!mouseIsPressed) {
         pet.offsetX++;
     }
