dccfc3ba9b77c0ebcdb59c9ed21c0d1031b2440b
ian-hinton <ian_hinton@yahoo.com>
2022-02-17 13:35:10 -0500

    comments added

--- Labs/Lab5/EX1/js/app.js
+++ Labs/Lab5/EX1/js/app.js
@@ -1,12 +1,20 @@
+//Ian Hinton
+//N220
+//17FEB2022
+//This program creates a square and a button
+//when button is clicked, square increases by 10%
+
+//initiate div
 let size = 100;
 let dvSquare = document.getElementById("square");
 
+//style div
 dvSquare.style.backgroundColor = "#00FF00";
 dvSquare.style.width = "100px";
 dvSquare.style.height = "100px";
 
 
-
+//grow function
 function grow() {
     size = size * 1.1;
     dvSquare.style.width = size + "px";
