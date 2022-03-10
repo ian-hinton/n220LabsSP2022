5cf7e6824ad61a839d094a7d86ec75bde1e7e25e
ian-hinton <ian_hinton@yahoo.com>
2022-03-10 12:20:55 -0500

    fc

---
+++ Labs/in-class work/mar10/js/app.js
@@ -0,0 +1,20 @@
+let txtName = document.getElementById("txtName");
+
+
+function remove() {
+    let userTxt = txtName.value;
+    let char = userTxt[i];
+    let word;
+    let none;
+    let some;
+
+    for (var i = 0; i < userTxt.length; i++) {
+        if (char == "a", "e", "i", "o", "u", "y") {
+            word = none;
+        } else {
+            word = some;
+        }
+    }
+    console.log(some);
+
+}
---
+++ Labs/in-class work/mar8/js/app.js
@@ -0,0 +1,9 @@
+let txtName = document.getElementById("txtName");
+
+function doubles() {
+    let userName = txtName.value;
+    let doubled;
+    //userName += userName + userName;
+    doubled = userName + userName;
+    console.log(doubled);
+}
