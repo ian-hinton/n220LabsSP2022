9ca3f47d03ceb9d133c7db74a69a58393b5d50f2
ian-hinton <ian_hinton@yahoo.com>
2022-03-24 05:03:21 -0500

    comments

--- Labs/Lab8/EX1/js/app.js
+++ Labs/Lab8/EX1/js/app.js
@@ -1,6 +1,14 @@
+//Ian Hinton
+//24Mar22
+//N220
+//This program creates an input field and button
+//When filled and pressed, "Hello" + inputted First Name is displayed
+
+//link java with html
 let Name = document.querySelector("#Name");
 let Page = document.querySelector("#Page");
 
+//function to write hello + name
 function Hello() {
     Page.innerHTML = "Hello " + Name.value;
 }
